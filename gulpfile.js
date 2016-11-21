443dc78dd4caf4f423f73d88db3b763be747397a
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2016-11-21 06:49:09 -0800

    Fixed gulpfile on Win

--- gulpfile.js
+++ gulpfile.js
@@ -97,12 +97,16 @@ gulp.task('serve', ['assemble'], function () {
   });
 
   gulp.watch('sass/**/*.{scss,sass}', ['style']);
-  gulp.watch('./*.html', ['copy-html']);
+  gulp.watch('*.html').on('change', (e) => {
+    if (e.type !== 'deleted') {
+      gulp.start('copy-html');
+    }
+  });
   gulp.watch('js/**/*.js', ['scripts']).on('change', server.reload);
 });
 
 gulp.task('assemble', ['clean'], function () {
-  gulp.start('copy', 'style');
+  gulp.start('copy');
 });
 
 gulp.task('build', ['assemble', 'imagemin']);
