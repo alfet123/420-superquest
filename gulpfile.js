423dbd0f027edd28700d7d078921a48bf47dda98
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2016-11-21 16:01:25 +0300

    use webpack for modules loading

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('babel-register');
 const del = require('del');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
@@ -13,8 +12,7 @@ const mqpacker = require('css-mqpacker');
 const minify = require('gulp-csso');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
+const webpack = require('gulp-webpack');
 
 gulp.task('style', function () {
   gulp.src('sass/style.scss')
@@ -42,9 +40,22 @@ gulp.task('style', function () {
 gulp.task('scripts', function () {
   return gulp.src('js/main.js')
     .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
+    .pipe(webpack({
+      devtool: 'source-map',
+      module: {
+        loaders: [{
+          test: /.js?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015']
+          }
+        }]
+      },
+      output: {
+        filename: 'main.js'
+      }
+    }))
     .pipe(gulp.dest('build/js/'));
 });
 
