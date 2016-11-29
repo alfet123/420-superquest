e909298651ad13d5b4d8e3ec8daf2def8b2558b7
Igor Alexeenko <alexeenko.igor@gmail.com>
2016-11-29 10:32:03 +0300

    Use loop to draw lives

--- js/game/header.js
+++ js/game/header.js
@@ -2,14 +2,17 @@ const drawHeart = (full) => {
   return `<span class="heart__${full ? `full` : `empty`}">${full ? `&#9829;` : `&#9825;`}</span>`;
 };
 
+
+const drawHearts = (lives) => {
+  return new Array(3).fill(0).map((it, i) => drawHeart(i + 1 >= lives)).join('');
+};
+
+
 export default (data) => {
   return `
 <header class="header">
   <div>Мир: ${data.level}</div>
-  <div>Жизни: ${drawHeart(data.lives > 2)}
-              ${drawHeart(data.lives > 1)}
-              ${drawHeart(data.lives > 0)}
-  </div>
+  <div>Жизни: ${drawHearts(data.lives)}</div>
   <div>Время: ${data.time}</div>
 </header>`;
 };
