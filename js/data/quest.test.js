0d85e612040a3b751fe609cbda43ca48de4a45aa
Igor Alexeenko <alexeenko.igor@gmail.com>
2016-11-29 21:44:19 +0300

    Tests for lives method

--- js/data/quest.test.js
+++ js/data/quest.test.js
@@ -1,10 +1,19 @@
 import assert from 'assert';
-import {changeLevel} from './quest';
+import {initialGame, setLives} from './quest';
 
-describe('Changing level', () => {
-  it('changeLevel should set passed level', () => {
-    assert.deepEqual(changeLevel({}, 1), {
-      level: 1
+
+describe('Game parameters', () => {
+  describe('Character lives', () => {
+    describe('Setting', () => {
+      it('Number of character\'s lives successfuly changes', () => {
+        assert.equal(setLives(initialGame, 3).lives, 3);
+      });
+    });
+
+    describe('Failures', () => {
+      it('setLives throws an error if negative value passed', () => {
+        assert.throws(() => setLives(initialGame, -1));
+      });
     });
   });
 });
