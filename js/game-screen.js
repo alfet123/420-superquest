7445e58a338c4a3549c844f61fbb65b3231ccfd5
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2016-12-02 13:06:38 +0300

    get rid of game-view

--- js/game-screen.js
+++ js/game-screen.js
@@ -1,60 +1,100 @@
-import {changeView} from './util';
+import {changeView, createElement} from './util';
 import {Result} from './data/quest-data';
-
-import GameView from './game/game-view';
 
 import showStats from './stats-screen';
 import QuestModel from './data/quest-model';
+import HeaderView from './game/header-view';
+import LevelView from './game/level-view';
+import GameOverView from './game/gameover-view';
 
 const questModel = new QuestModel();
+const root = createElement(``);
 
-const gameView = new GameView(questModel);
-gameView.onAnswer = (answer) => {
-  endGame();
-  switch (answer.result) {
-    case Result.NEXT:
-      questModel.nextLevel();
-      beginGame();
-      break;
-    case Result.DIE:
-      questModel.die();
-      gameView.endGame(false, !(questModel.isDead()));
-      break;
-    case Result.WIN:
-      gameView.endGame(true, false);
-      break;
-    default:
-      throw new Error(`Unknown result: ${answer.result}`);
+const game = {
+  header: new HeaderView(questModel.state),
+  content: new LevelView(questModel.getCurrentLevel()),
+  interval: null,
+
+  stopGame()  {
+    clearInterval(game.interval);
+  },
+
+  startGame() {
+    game.update();
+
+    game.interval = setInterval(() => {
+      questModel.tick();
+      game.updateHeader();
+    }, 1000);
+  },
+
+  onAnswer(answer) {
+    game.stopGame();
+    switch (answer.result) {
+      case Result.NEXT:
+        questModel.nextLevel();
+        game.startGame();
+        break;
+      case Result.DIE:
+        questModel.die();
+        game.endGame(false, !(questModel.isDead()));
+        break;
+      case Result.WIN:
+        game.endGame(true, false);
+        break;
+      default:
+        throw new Error(`Unknown result: ${answer.result}`);
+    }
+  },
+
+  onRestart(continueGame) {
+    if (!continueGame) {
+      questModel.restart();
+    }
+    game.startGame();
+  },
+
+  onExit() {
+    showStats(questModel.state);
+  },
+
+  updateHeader() {
+    const header = new HeaderView(questModel.state);
+    root.replaceChild(header.element, game.header.element);
+    game.header = header;
+  },
+
+  update() {
+    game.updateHeader();
+
+    const level = new LevelView(questModel.getCurrentLevel());
+    level.onAnswer = game.onAnswer;
+    game.changeContentView(level);
+    level.focus();
+  },
+
+
+  endGame(win, canContinue) {
+    const gameOver = new GameOverView(win, canContinue);
+    gameOver.onRestart = game.onRestart;
+    gameOver.onExit = game.onExit;
+
+    game.changeContentView(gameOver);
+    game.updateHeader();
+  },
+
+  changeContentView(view) {
+    root.replaceChild(view.element, game.content.element);
+    game.content = view;
   }
-};
-gameView.onRestart = (continueGame) => {
-  if (!continueGame) {
-    questModel.restart();
-  }
-  beginGame();
+
 };
 
-gameView.onExit = () => {
-  showStats(questModel.state);
-};
-
-let interval = null;
-
-const endGame = () => {
-  clearInterval(interval);
-};
-
-const beginGame = () => {
-  gameView.update();
-
-  interval = setInterval(() => {
-    questModel.tick();
-    gameView.updateHeader();
-  }, 1000);
-};
+root.appendChild(game.header.element);
+root.appendChild(game.content.element);
 
 export default () => {
   questModel.restart();
-  beginGame();
-  changeView(gameView.element);
+  game.startGame();
+  changeView(root);
 };
